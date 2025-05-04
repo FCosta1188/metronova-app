238a5c628c3152725036094c537690c9dba14b4d
FCosta1188 <francesco.costa.1188@gmail.com>
2025-05-04 12:47:35 +0200

    Added ktor starter to backend.

---
+++ metronova-backend/src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.fcosta
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
